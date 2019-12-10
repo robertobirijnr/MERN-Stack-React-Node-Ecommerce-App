f00129992ab2a1f8ca39cf2d28aa307aeda188d2
robertobirijnr <robertobirijnr@hotmail.com>
2019-12-10 12:53:30 +0000

    showing products on shop page

--- frontend/src/components/apiHome/apiHome.js
+++ frontend/src/components/apiHome/apiHome.js
@@ -21,3 +21,23 @@ export const getCategories = () => {
     .catch(err => console.log(err));
 };
 
+export const getFilteredproducts = (skip,limit ,filters = []) =>{
+  const data = {
+    skip,limit,filters
+  }
+  return fetch(`http://localhost:8000/api/products/by/search`,{
+    method:'POST',
+    headers:{
+      Accept:'application/json',
+      "Content-Type":'application/json'
+    },
+    body:JSON.stringify(data)
+
+  })
+  .then(response=>{
+    return response.json();
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+}
--- frontend/src/components/shop.js
+++ frontend/src/components/shop.js
@@ -1,24 +1,38 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
-import { getCategories } from "./apiHome/apiHome";
+import { getCategories, getFilteredproducts } from "./apiHome/apiHome";
 import Checkbox from "./checkBox";
 import { prices } from "./fixedPrices";
 import RadioBox from "./radioBox";
-// import Card from  './card';
+import Card from "./card";
 
 const Shop = () => {
   const [myFilters, setMyFilters] = useState({
     filters: { category: [], price: [] }
   });
   const [categories, setCategories] = useState([]);
-  const [error, setErrors] = useState(false);
+  const [error, setError] = useState(false);
+  const [limit, setLimit] = useState(6);
+  const [skip, setSkip] = useState(0);
+  const [filteredResults, setfilteredResults] = useState([]);
 
   const loadCategories = () => {
     getCategories().then(data => {
       if (data.error) {
-        setErrors(data.error);
+        setError(data.error);
       } else {
         setCategories(data);
+      }
+    });
+  };
+
+  const loadFilteredResults = newFilters => {
+    console.log(newFilters);
+    getFilteredproducts(skip, limit, newFilters).then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setfilteredResults(data.data);
       }
     });
   };
@@ -27,22 +41,19 @@ const Shop = () => {
 
   useEffect(() => {
     loadCategories();
+    loadFilteredResults(skip, limit, myFilters.filters);
   }, []);
 
   const handleFilters = (filters, filterBy) => {
     // console.log("Shop", filters, filterBy);
     const newFilters = { ...myFilters };
     newFilters.filters[filterBy] = filters;
-    if (filterBy == "price") {
+    if (filterBy === "price") {
       let priceValues = handlePrice(filters);
       newFilters.filters[filterBy] = priceValues;
     }
     loadFilteredResults(myFilters.filters);
     setMyFilters(newFilters);
-  };
-
-  const loadFilteredResults = newFilters => {
-    console.log(newFilters);
   };
 
   const handlePrice = value => {
@@ -65,7 +76,7 @@ const Shop = () => {
     >
       <div className="row">
         <div className="col-4">
-          <h4>Filter By Id</h4>
+          <h4>Filter by categories</h4>
           {/* {JSON.stringify(categories)} */}
           <ul>
             <Checkbox
@@ -75,7 +86,6 @@ const Shop = () => {
           </ul>
 
           <h4>Filter by price range</h4>
-          {/* {JSON.stringify(categories)} */}
           <div>
             <RadioBox
               prices={prices}
@@ -83,7 +93,15 @@ const Shop = () => {
             />
           </div>
         </div>
-        <div className="col-8">{JSON.stringify(myFilters)}</div>
+        <div className="col-8">
+          {/* {JSON.stringify(filteredResults)} */}
+          <h2 className="mb-4">Products</h2>
+          <div className="row">
+            {filteredResults.map((product, i) => (
+              <Card key={i} product={product} />
+            ))}
+          </div>
+        </div>
       </div>
     </Layout>
   );
