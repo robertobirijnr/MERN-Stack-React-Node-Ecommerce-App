437784af07c3d7fe8661bafb908eb878d0a8d191
robertobirijnr <robertobirijnr@hotmail.com>
2019-12-19 14:07:27 +0000

    updating user profile

--- frontend/src/components/user/profile.js
+++ frontend/src/components/user/profile.js
@@ -1,10 +1,112 @@
-import React from "react";
-// import Layout from "../Layout";
-// import { isAuthenticated } from "../auth/index";
-// import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { getUserProfile, updateUserProfile, updateUser } from "./apiUser";
+import Layout from "../Layout";
+import { isAuthenticated } from "../auth/index";
+import { Redirect } from "react-router-dom";
 
-const Profile = () => {
-  return <h2>User Profile</h2>;
+const Profile = props => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: false,
+    success: false
+  });
+
+  const { token } = isAuthenticated();
+  const { name, email, password, error, success } = values;
+
+  const init = userId => {
+    getUserProfile(userId, token).then(data => {
+      if (data.error) {
+        setValues({ ...values, error: true });
+      } else {
+        setValues({ ...values, name: data.name, email: data.email });
+      }
+    });
+  };
+
+  useEffect(() => {
+    init(props.match.params.userId);
+  }, []);
+
+  const handleChange = name => e => {
+    setValues({ ...values, error: false, [name]: e.target.value });
+  };
+
+  const clickSubmit = e => {
+    e.preventDefault();
+    updateUserProfile(props.match.params.userId, token, {
+      name,
+      email,
+      password
+    }).then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        updateUser(data, () => {
+          setValues({
+            ...values,
+            name: data.name,
+            email: data.email,
+            success: true
+          });
+        });
+      }
+    });
+  };
+
+  const redirectUser = success => {
+    if (success) {
+      return <Redirect to="/cart" />;
+    }
+  };
+  const profileUpdate = (name, email, password) => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          type="text"
+          onChange={handleChange("name")}
+          className="form-control"
+          value={name}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          type="emil"
+          onChange={handleChange("email")}
+          className="form-control"
+          value={email}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          type="password"
+          onChange={handleChange("password")}
+          className="form-control"
+          value={password}
+        />
+      </div>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+
+  return (
+    <Layout
+      title="Profile"
+      description="Update your Profile"
+      className="container-fluid"
+    >
+      <h2>Profile update</h2>
+      {profileUpdate(name, email, password)}
+      {redirectUser(success)}
+    </Layout>
+  );
 };
 
 export default Profile;
