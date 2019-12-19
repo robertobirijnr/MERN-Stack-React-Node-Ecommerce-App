85e3883f3420b36d872dcbf6f94868d2da6be38e
robertobirijnr <robertobirijnr@hotmail.com>
2019-12-19 18:25:41 +0000

    show update and delete

--- frontend/src/components/admin/manageProducts.js
+++ frontend/src/components/admin/manageProducts.js
@@ -1,9 +1,35 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import Layout from "../Layout";
-// import { isAuthenticated } from "../auth/index";
+import {getProducts,deleteProduct} from './apiAdmin'
+import { isAuthenticated } from "../auth/index";
 // import { Link } from "react-router-dom";
 
 const ManageProducts = () => {
+    const [products , setProducts] = useState([])
+    const {user,token} = isAuthenticated()
+
+    const loadProducts = ()=>{
+        getProducts().then(data=>{
+            if(data.error){
+                console.log(data.error)
+            }else{
+                setProducts(data)
+            }
+        })
+    }
+    const destroy = productId =>{
+        deleteProduct(productId,user._id,token).then(data=>{
+            if(data.error){
+                console.log(data.error)
+            }else{
+                loadProducts()
+            }
+        })
+    }
+
+    useEffect(()=>{
+        loadProducts()
+    },[])
     return (
         <Layout
           title="Manage products"
@@ -12,7 +38,15 @@ const ManageProducts = () => {
         >
           
          <div className="row">
-            <div></div>
+            <div className="col-12">
+                <ul className="list-group">
+                 {products.map((p,i)=>(
+                       <li key={i} className="list-group-item d-flec justify-content-betwwen align-items-center">
+                          <strong> {p.name}</strong>
+                       </li> 
+                 ))}
+                </ul>
+            </div>
          </div>
          
         </Layout>
